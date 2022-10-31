c1eea303f7078b650d0e79be66a5fdb7e4fbfbe5
Rani Patel <ranip99@cs.utexas.edu>
2022-10-31 16:23:14 -0500

    changing website

--- ranip99/HW8/hw8.js
+++
@@ -1,9 +0,0 @@
-
-images = ["GalaxyCluster.jpg", "InteractingGalaxies.jpg", "M51.jpg", "M104.jpg", "NGC1300.jpg", "NGC6217.jpg"];
-alt = ["Galaxy Cluster", "Interacting Galaxies", "M 51", "M 104", "NGC 1300", "NGC 6217"];
-
-window.onload = function load() {
-  idx = Math.trunc (Math.random() * images.length)
-  document.getElementById('galaxy').src = images[idx];
-  document.getElementById('caption').innerHTML = alt[idx];
-}
